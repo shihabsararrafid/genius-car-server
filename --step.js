54f09b9c2b4790fbb97f039c9f4389c0a0c470fe
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-05-11 15:53:09 +0600

    connnected with mongo db

---
+++ --step.js
@@ -0,0 +1,4 @@
+/**
+ * Starting with express
+ * 1.
+*/
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT||5000;
 const cors = require('cors');
-
+require('dotenv').config();
 // using middleware
 
 app.use(cors());
 app.use(express.json());
+
+// using mongo db 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d7471.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("Connected with mongo db");
+  client.close();
+});
 
 // testing the server whether it is running or not
 
