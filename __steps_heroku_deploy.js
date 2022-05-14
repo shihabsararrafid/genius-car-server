b08a68a0bedb0cce85a8dde3610e56208899cd95
Shihab Sarar Rafid <shrafid.532@gmail.com>
2022-05-14 12:22:32 +0600

    ready for heroku commit

---
+++ __steps_heroku_deploy.js
@@ -0,0 +1,24 @@
+/**
+ * ---------------------------------
+ * One time for user
+ * ---------------------------------
+ * 1.Heroku sign up
+ * 2.Verify Email
+ * 3.Install Heroku CLI
+ * 4.Heroku login
+ * 
+ * 
+ * -----------------------------------------
+ * Everytime before deployment
+ * -------------------------
+ * 1.Check the server:
+ * 1.1. Check whether it is working locally
+ * 1.2. Check whether there is a process.env.port variable in the port variable
+ * 1.3.Must be a script with start and start-dev which will contain "node index.js" and "nodemon index.js"
+ * 1.4. Git commit before push to heroku
+ * 1.5. run the command "heroku create"
+ * 1.6. run the command "git push heroku main"
+ *
+
+ * 
+*/
